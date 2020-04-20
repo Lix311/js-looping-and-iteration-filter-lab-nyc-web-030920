63b699643f780e62789bf581db54c112a28469d2
Lix311 <alex.sterrantino@gmail.com>
2020-04-19 22:04:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file
+// function findMatching(collection,string){
+//     let name_array = []
+//     for(const user of collection){
+//         if (user.toLowerCase() === string.toLowerCase()){
+//             name_array.push(user)
+//         }
+//     }
+//     return name_array
+// }
+
+function findMatching (list, name) {
+    return list.filter(function (driverName) {
+      return driverName.toLowerCase() === name.toLowerCase();
+    });
+  }
+
+  function fuzzyMatch (list, parName) {
+    let lenOfName = parName.length
+    return list.filter(function (driverName) {
+      return driverName.slice(0,lenOfName) === parName;
+    });
+  }
+
+  function matchName (list, name) {
+    return list.filter(function (driverName) {
+      return driverName.name === name;
+    });
+  }
